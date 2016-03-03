e1400e97c8d369ef0c12f396eebb66c2ba97040c
amandeep1509 <asing609@my.centennialcollege.ca>
2016-03-02 20:14:40 -0400

    final commit

--- Assets/_Scripts/GameController.cs
+++ Assets/_Scripts/GameController.cs
@@ -1,14 +1,71 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class GameController : MonoBehaviour {
 	// PUBLIC INSTANCE VARIABLES
 	public int tankCount;
 	public GameObject tank;
+    public Text ScoreLabel;
+    public Text LivesLabel;
+    public Text GameOverLabel;
+    public Text HighScoreLabel;
+    public Button RestartButton;
+
+
+    public PlayerController player;
+    public EnemyController enemy;
+
+    // PRIVATE INSTANCE VARIABLES
+    private int _scoreValue;
+    private int _livesValue;
+
+    // PUBLIC ACCESS METHODS 
+    public int ScoreValue
+    {
+        get
+        {
+            return _scoreValue;
+
+        }
+        set
+        {
+            this._scoreValue = value;
+            Debug.Log(this._scoreValue);
+            this.ScoreLabel.text = "Score: " + this._scoreValue;
+
+        }
+    }
+    public int LivesValue
+    {
+        get
+        {
+            return _livesValue;
+        }
+        set
+        {
+            this._livesValue = value;
+            Debug.Log(this._livesValue);
+            if (this._livesValue <= 0)
+            {
+                this._endGame();
+            }
+            else
+            {
+                this.LivesLabel.text = "Lives: " + this._livesValue;
+            }
+        }
+    }
 	
 	// Use this for initialization
 	void Start () {
 		this._GenerateTanks ();
+        this.ScoreValue = 0;
+        this.LivesValue = 5;
+        this.GameOverLabel.gameObject.SetActive(false);
+        this.HighScoreLabel.gameObject.SetActive(false);
+        this.RestartButton.gameObject.SetActive(false);
 	}
 	
 	// Update is called once per frame
@@ -22,4 +79,27 @@ public class GameController : MonoBehaviour {
 			Instantiate(tank);
 		}
 	}
+    private void _endGame()
+    {
+
+        this.HighScoreLabel.text = "High Score: " + this._scoreValue;
+        this.GameOverLabel.gameObject.SetActive(true);
+        this.HighScoreLabel.gameObject.SetActive(true);
+        this.LivesLabel.gameObject.SetActive(false);
+        this.ScoreLabel.gameObject.SetActive(false);
+        this.player.gameObject.SetActive(false);
+        this.enemy.gameObject.SetActive(false);
+        this.RestartButton.gameObject.SetActive(true);
+
+    }
+
+    // PUBLIC METHODS
+
+    //Method to restart the game when restart button clicked
+    public void RestartButtonClick()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+
+    }
+
 }
---
+++ Assets/_Scripts/PlayerCollider.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerCollider : MonoBehaviour
+{
+
+    public GameController gameController;
+    public EnemyController enemy;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    //Method to detect collisions
+    public void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log(this.gameController.LivesValue);
+
+        if (other.gameObject.CompareTag("Enemy"))
+        {
+
+            this.gameController.LivesValue -= 1;
+            this.gameController.ScoreValue += 10;
+            this.enemy._Reset();
+
+        }
+
+
+
+    }
+
+}
